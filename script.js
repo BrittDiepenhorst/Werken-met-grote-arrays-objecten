6ed20372d068aa4b6017a7eac088392425df576d
Britt Diepenhorst <brittdiepenhorst@hotmail.com>
2021-07-09 10:58:19 +0200

    Werken met grote arrays en objecten

--- script.js
+++ script.js
@@ -13,10 +13,9 @@ const addDataToDom = (array, list) => {
         const listItem = document.createElement('li');
         list.appendChild(listItem);
         listItem.innerHTML = element;
-        // foto toevoegen lukt niet, waarom niet? 
         // const photoImg = document.createElement("img");
         // listItem.appendChild(photoImg);
-        // photoImg.setAttribute('src', element.photo);
+        // photoImg.setAttribute('src', element[2]);
         // listItem.appendChild(photoImg);
     })
 };
@@ -36,20 +35,18 @@ buttonLandList.addEventListener("click", function () {
 // Maak een lijst van mensen voor- en achternaam en hun foto, sorteer de lijst op voornaam, vrouw, ouder zijn dan 30 (1990 of ouder), een steenbok zijn (jarig van 22 december t/m 19 januari)
 buttonSteenbokVrouwen.addEventListener("click", function () {
     const MakingSteenBokList = () => {
-        let telwaarde = 0;
         let filteredArray = [];
         randomPersonData.forEach((element) => {
             const birthdayDay = parseInt(element.birthday.dmy.split("/")[0]);
             const birthdayMonth = parseInt(element.birthday.dmy.split("/")[1]);
             const birthdayYear = parseInt(element.birthday.dmy.split("/")[2]);
             if (element.gender === "female" &&
-                // element.age > 30 &&; waarom werkt dit niet en birthdayYear wel? 
                 birthdayMonth == 12 && birthdayDay > 21 && birthdayYear < 1990 ||
                 birthdayMonth == 01 && birthdayDay < 20 && birthdayYear < 1990) {
-                filteredArray[telwaarde] = element.name + ' ' + element.surname + ' ' + element.photo;
-                telwaarde += 1;
+                filteredArray.push([element.name, element.surname])
             }
         })
+
         const sorteredFilteredArray = filteredArray.sort();
 
         addDataToDom(sorteredFilteredArray, steenbokList);
